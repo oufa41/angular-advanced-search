3869b787746fa8d853ec2316146952c8a365b01c
oufa41 <ahmed.m.raouf95@gmail.com>
2020-09-01 12:59:27 +0200

    remove test and rebuildv2

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function(config) {
     config.set({
         basePath: '',
@@ -15,6 +16,7 @@ module.exports = function(config) {
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
+
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, './coverage/angular-advanced-search'),
             reports: ['html', 'lcovonly', 'text-summary'],
