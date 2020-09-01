ba2540f5ebf83ca851d5c1c62082f654c29dcff4
oufa41 <ahmed.m.raouf95@gmail.com>
2020-09-01 18:03:48 +0200

    new karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,72 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// module.exports = function(config) {
+//     config.set({
+//         basePath: '',
+//         frameworks: ['jasmine', '@angular-devkit/build-angular'],
+//         plugins: [
+//             require('karma-jasmine'),
+//             require('karma-chrome-launcher'),
+//             require('karma-jasmine-html-reporter'),
+//             require('karma-coverage-istanbul-reporter'),
+//             require('@angular-devkit/build-angular/plugins/karma')
+//         ],
+//         client: {
+//             clearContext: false // leave Jasmine Spec Runner output visible in browser
+//         },
+
+//         coverageIstanbulReporter: {
+//             dir: require('path').join(__dirname, './coverage/angular-advanced-search'),
+//             reports: ['html', 'lcovonly', 'text-summary'],
+//             fixWebpackSourcePaths: true
+//         },
+//         reporters: ['progress', 'kjhtml'],
+//         port: 9876,
+//         colors: true,
+//         logLevel: config.LOG_INFO,
+//         autoWatch: true,
+
+//         singleRun: true,
+//         customLaunchers: {
+//             ChromeHeadlessNoSandbox: {
+//                 base: 'ChromeHeadless',
+//                 flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions', '--remote-debugging-port=9222']
+//             }
+//         },
+//         browsers: ['Chrome'],
+//         restartOnFileChange: true,
+//         failOnEmptyTestSuite: false
+//     });
+// };
+
 module.exports = function(config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        frameworks: ['jasmine', '@angular/cli'],
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            require('karma-junit-reporter'),
+            require('@angular/cli/plugins/karma')
         ],
-        client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        reporters: ['junit'],
+            
+        junitReporter: {
+            outputDir: 'karma-results',
+                
+            outputFile: 'karma-results.xml'    
         },
-
-        coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, './coverage/angular-advanced-search'),
-            reports: ['html', 'lcovonly', 'text-summary'],
-            fixWebpackSourcePaths: true
+        angularCli: {
+            environment: 'dev',
         },
-        reporters: ['progress', 'kjhtml'],
         port: 9876,
-        colors: true,
         logLevel: config.LOG_INFO,
-        autoWatch: true,
-
-        singleRun: true,
-        customLaunchers: {
-            ChromeHeadlessNoSandbox: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions', '--remote-debugging-port=9222']
-            }
-        },
-        browsers: ['Chrome'],
-        restartOnFileChange: true,
-        failOnEmptyTestSuite: false
+        browsers: ['ChromeHeadless'],
+            
+        autoWatch: false,
+            
+        singleRun: true    
     });
 };
