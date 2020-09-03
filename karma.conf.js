adba11caa716c08fd0a0e9f67711bc65e9ab0c5e
oufa41 <ahmed.m.raouf95@gmail.com>
2020-09-03 13:40:25 +0200

    reverting

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
             fixWebpackSourcePaths: true
         },
         reporters: ['progress', 'kjhtml'],
+        port: 9876,
         //  colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
