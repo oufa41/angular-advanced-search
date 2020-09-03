7c7b7530d5fa7bfdfb537ab37abaf5b4d8dbd2c6
oufa41 <ahmed.m.raouf95@gmail.com>
2020-09-03 02:23:11 +0200

    junit for karma

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
                     '--disable-gpu',
                     '--no-sandbox',
                     '--disable-web-security',
-
+                    '--remote-debugging-port=9222',
                 ]
             }
         },
