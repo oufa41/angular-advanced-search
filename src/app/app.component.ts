4d23ec14ca62e07bf0d37e87bee3201fbae9ae35
ABD EL RAOUF Ahmed <Ahmed.ABDELRAOUF@cegedim.com>
2020-09-13 11:57:42 +0200

    working with topbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Data } from './data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  implements OnInit{
-    title = 'angular-advanced-search';
-
+export class AppComponent implements OnInit {
+  title = 'angular-advanced-search';
+  data: Data[] = [];
+  count = 0;
+  constructor(private changeDetection: ChangeDetectorRef) {
+    this.fillInitialArray();
+  }
   ngOnInit(): void {
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    
+  }
+  changeFromParent(): void {
+    const newData: Data[] = [];
+    for (const data of this.data) {
+      newData.push(new Data(data.id, data.name));
+    }
+    const messageContent: string = Math.random().toString(36).substring(7);
+    const messageId: number = Math.random();
+    newData.push(new Data(messageId, messageContent));
+    this.data = [];
+    this.data = newData;
+    // this.data.push(new Data(this.count++, 'ahmed'));
+    // this.data = this.data.slice();
+    console.log(this.data);
+    // setTimeout(() => {
+    //   this.data.push(new Data (200,'Ahmed'));
+    //   console.log("/// timeout executed");
+    //  // this.changeDetection.detectChanges();
+    // }, 5000);
+  }
+
+  private fillInitialArray() {
+    this.data.push(new Data(0, 'Hi'));
+    this.data.push(new Data(1, 'Hi !!!'));
+    this.data.push(new Data(2, 'Any plans today ?'));
+    this.data.push(new Data(3, 'Noop'));
+    this.data.push(new Data(4, 'Just optimizing some arrays :P'));
   }
 }
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, OnInit, OnChanges, Input, ChangeDetectorRef } from '@angular/core';
+import { Data } from '../data';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TopBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TopBarComponent implements OnInit, OnChanges {
+  @Input() data: Data[];
+  constructor(private changeDetection: ChangeDetectorRef) {
   }
 
+
+  ngOnInit(): void {
+    console.log('onIt');
+    setTimeout(() => {
+      this.data.push(new Data (100,'heikal'));
+      console.log("/// timeout executed");
+     // this.changeDetection.detectChanges();
+    }, 5000);
+  }
+  ngOnChanges(): void {
+    console.log(this.data);
+  }
+  dataByName(index, data) {
+    return data.id;
+  }
 }
